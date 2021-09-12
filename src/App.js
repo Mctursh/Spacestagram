c230496013c3ed5d359192f925f34e67f93dfa95
mctursh <adenugaayoadehenry63@gmail.com>
2021-09-12 23:05:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SpaceStagram</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// https://api.nasa.gov/planetary/apod?api_key=raGisHkQN11NgNhPxcUPvMbCIbxSCc3yooEkPh8Z&count=5\
+
+// Account Email: Adenugaayoadehenry63@gmail.com
+// Account ID: 97fd944e-9fa1-474b-be26-100753498199
